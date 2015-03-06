47f5a44efdcb3de4995e5564675376334deae708
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-06 11:12:28 +0900

    .

--- java01t/src/step02/Test02.java
+++ java01t/src/step02/Test02.java
@@ -1,10 +1,36 @@
 package step02;
 
+//형식을 갖춰 출력하기
+//printf(), format()
+//1)printf("형식 문자열", 변수, 변수, ...)
+
 public class Test02 {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    //printf(포맷, 값들...)
+    //포맷 문법: %[argument_index$][flags][width][.precision]conversion
+    //1. conversion(대소문자 모두 허용)
+    // b - boolean
+    // h - 16진수
+    // s - 문자열
+    // c - 유니코드 문자
+    // d - 10진수
+    // o - 8진수
+    // x - 16진수
+    // n - 줄 바꿈
+    System.out.printf("국어: %d\n영어: %d\n", 100, 90);
+    System.out.printf("%d, %o, %x, %h%n", 13, 13, 13, 13);
+    System.out.printf("%s, %c, %d%n", "오호라", 0xAC00, 0xAC00);
 
+    //
   }
 
 }
+
+
+
+
+
+
+
+
