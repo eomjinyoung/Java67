37c0d4e88d1d519584c0e400476a56dc77320fd1
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-17 11:31:06 +0900

    .

--- java01t/src/step11/ex1/Member.java
+++ java01t/src/step11/ex1/Member.java
@@ -1,8 +1,8 @@
 package step11.ex1;
 
 // Object의 toString() 메서드
-// - 리턴 값: 패키지명.클래스명@구분자
-// - 구분자 : 인스턴스 마다 고유의 ID 값을 부여한다.
+// - 리턴 값: 패키지명.클래스명@구분자(해시값)
+// - 구분자 : 인스턴스 마다 고유의 ID 값을 부여한다. hashCode()의 리턴 값.
 public class Member {
   String name;
   String id;
