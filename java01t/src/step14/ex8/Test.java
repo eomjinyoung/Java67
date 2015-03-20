f4895bbb2b9d45a72449195a96743598fb5cb718
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-20 16:15:48 +0900

    .

---
+++ java01t/src/step14/ex8/Test.java
@@ -0,0 +1,16 @@
+package step14.ex8;
+
+// LinkedList 알고리즘 구현하기
+// 왜 배열을 사용하지 않고 링크드리스트라는 알고리즘을 사용하는가?
+// => 배열은 한 번 크기를 정하면 바꿀 수 없다.
+// => LinkedList는 저장소의 크기를 바꿀 수 있다.
+// 원리
+// => 열차의 객차를 추가하거나 제거하는 방법 
+public class Test {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
