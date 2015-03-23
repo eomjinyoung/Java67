b24e079404fb95c86141d583c96d32c478c94918
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-23 12:28:59 +0900

    .

--- java01t/src/step14/ex9/MyStack.java
+++ java01t/src/step14/ex9/MyStack.java
@@ -1,16 +1,26 @@
 package step14.ex9;
 
 public class MyStack {
-  Item start;
-  Item end;
+  Item cursor;
   int length;
   
   public void push(Object value) {
-    
+    Item newItem = new Item();
+    newItem.value = value;
+    newItem.next = cursor;
+    cursor = newItem;
+    length++;
   }
   
   public Object pop() {
-    return null;
+    if (cursor == null) {
+      return null;
+    }
+    
+    Object value = cursor.value;
+    cursor = cursor.next;
+    length--;
+    return value;
   }
 
   public int size() {
--- java01t/src/step14/ex9/Test.java
+++ java01t/src/step14/ex9/Test.java
@@ -6,17 +6,18 @@ public class Test {
 
   public static void main(String[] args) {
     MyStack stack = new MyStack();
-    stack.push("홍길동");
-    stack.push("임꺽정");
-    stack.push("유관순");
-    stack.push("안중근");
-    stack.push("윤봉길");
+    stack.push("홍길동"); System.out.println(stack.size());
+    stack.push("임꺽정"); System.out.println(stack.size());
+    stack.push("유관순"); System.out.println(stack.size());
+    stack.push("안중근"); System.out.println(stack.size());
+    stack.push("윤봉길"); System.out.println(stack.size());
     
     System.out.println(stack.size());
     
     Object value = null;
     while ((value = stack.pop()) != null) {
       System.out.println(value);
+      System.out.println(stack.size());
     }
 
   }
