d0192aa24a9841074147a18578af930ae9064a77
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-26 12:56:25 +0900

    .

---
+++ java01t/src/step17/ex3/Test1.java
@@ -0,0 +1,46 @@
+package step17.ex3;
+
+// 실습 목표: 기본 입력 (byte 단위)
+// 
+// 기본 입출력 개요
+// - byte 또는 char 단위로 입출력하기
+// - 도구: 
+//   1) InputStream/OutputStream 계열
+//   2) Reader/Writer 계열
+//
+// InputStream/OutputStream 계열
+// - byte 단위로 입출력 하는 클래스 => "byte stream class"
+//
+// Reader/Writer 계열
+// - char 단위로 입출력 하는 클래스 => "character stream class"
+// - 읽기: 파일(UTF-8) ---> JVM(Unicode2)
+// - 쓰기: JVM(Unicode) ---> 파일(UTF-8)
+// - 읽고 쓸 때 유니코드를 시스템 기본 문자 집합으로 변환하는 기능을 수행한다.
+//
+// 저장소 유형
+// - 메모리, 파일, 프로세스(실행 중인 프로그램) 
+// 
+// 저장소 유형에 따라서 데이터를 입출력하는 도구
+// 1) 메모리 
+//    - 
+// 2) 파일
+// 3) 프로세스
+
+public class Test1 {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
