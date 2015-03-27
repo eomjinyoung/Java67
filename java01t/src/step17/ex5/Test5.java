9ef28736ff1b9301eb425593cc43bfd652cea3a0
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-27 15:42:32 +0900

    .

---
+++ java01t/src/step17/ex5/Test5.java
@@ -0,0 +1,13 @@
+package step17.ex5;
+
+// 실습 목표: ObjectOutputStream/ObjectInputStream 사용하기
+// => 인스턴스의 내용을 출력하기
+
+public class Test5 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
