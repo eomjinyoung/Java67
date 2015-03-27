4376e48049044321c511fa35a71a2eaa6193f856
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-27 16:20:09 +0900

    .

--- java01t/src/step17/ex6/Score.java
+++ java01t/src/step17/ex6/Score.java
@@ -8,6 +8,13 @@ public class Score {
   int total;
   float average;
   
+  // 테스트 할 때 간단히 User 객체의 내용을 확인하기 위해.
+  @Override
+  public String toString() {
+    return "Score [name=" + name + ", kor=" + kor + ", eng=" + eng + ", math="
+        + math + ", total=" + total + ", average=" + average + "]";
+  }
+
   // 빈 인스턴스를 생성할 수 있도록 기본 생성자도 만든다.
   public Score() {}
   
@@ -19,4 +26,55 @@ public class Score {
     this.total = kor + eng + math;
     this.average = this.total / 3f;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getKor() {
+    return kor;
+  }
+
+  public void setKor(int kor) {
+    this.kor = kor;
+  }
+
+  public int getEng() {
+    return eng;
+  }
+
+  public void setEng(int eng) {
+    this.eng = eng;
+  }
+
+  public int getMath() {
+    return math;
+  }
+
+  public void setMath(int math) {
+    this.math = math;
+  }
+
+  public int getTotal() {
+    return total;
+  }
+
+  public void setTotal(int total) {
+    this.total = total;
+  }
+
+  public float getAverage() {
+    return average;
+  }
+
+  public void setAverage(float average) {
+    this.average = average;
+  }
+  
+  
+  
 }
--- java01t/src/step17/ex6/Test.java
+++ java01t/src/step17/ex6/Test.java
@@ -1,12 +1,72 @@
 package step17.ex6;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 // 실습 목표: ObjectOutputStream/ObjectInputStream 사용하기
 // => 인스턴스의 내용을 출력하기
-
+//
+// 1단계: DataOutputStream/DataInputStream을 사용하여 출력하기
 public class Test {
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+  static void writeScore() throws Exception {
+    Score[] scores = {
+        new Score("홍길동", 100, 100, 100),
+        new Score("임꺽정", 90, 90, 90),
+        new Score("유관순", 80, 80, 80)};
+    
+    DataOutputStream out = new DataOutputStream(
+        new BufferedOutputStream(
+            new FileOutputStream("temp/ex6.test.data")));
+    
+    out.writeInt(scores.length);
+    
+    for (Score score : scores) {
+      out.writeUTF(score.getName());
+      out.writeInt(score.getKor());
+      out.writeInt(score.getEng());
+      out.writeInt(score.getMath());
+      // 데이터를 가지고 계산을 수행해서 나온 값을 출력에서 제외하라!
+      // 데이터를 읽을 때 다시 계산을 수행하는 것이 데이터가 왜곡될 가능성을 줄이기 때문이다.
+    }
+    
+    out.close();
+  }
+  
+  static void readScore() throws Exception {
+    DataInputStream in = new DataInputStream(
+        new BufferedInputStream(
+            new FileInputStream("temp/ex6.test.data")));
+    
+    int len = in.readInt();
+    
+    Score score = null;
+    for (int i = 0; i < len; i++) {
+      score = new Score(
+          in.readUTF(), in.readInt(), in.readInt(), in.readInt());
+      System.out.println(score);
+    }
+    
+    in.close();
+  }
+  
+  public static void main(String[] args) throws Exception {
+    writeScore();
+    readScore();
+    
   }
 
 }
+
+
+
+
+
+
+
+
+
+
