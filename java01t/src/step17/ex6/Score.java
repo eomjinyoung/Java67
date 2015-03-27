898af4b8d25a591eac0b20908ff2fb26da68538d
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-27 15:47:02 +0900

    .

---
+++ java01t/src/step17/ex6/Score.java
@@ -0,0 +1,22 @@
+package step17.ex6;
+
+public class Score {
+  String name;
+  int kor;
+  int eng;
+  int math;
+  int total;
+  float average;
+  
+  // 빈 인스턴스를 생성할 수 있도록 기본 생성자도 만든다.
+  public Score() {}
+  
+  public Score(String name, int kor, int eng, int math) {
+    this.name = name;
+    this.kor = kor;
+    this.eng = eng;
+    this.math = math;
+    this.total = kor + eng + math;
+    this.average = this.total / 3f;
+  }
+}
--- java01t/src/step17/ex6/Test5.java
+++ java01t/src/step17/ex6/Test5.java
@@ -1,10 +1,9 @@
-package step17.ex5;
+package step17.ex6;
 
 // 실습 목표: ObjectOutputStream/ObjectInputStream 사용하기
 // => 인스턴스의 내용을 출력하기
 
 public class Test5 {
-
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
