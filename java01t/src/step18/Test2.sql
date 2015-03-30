caa37ba5f0efccce2cbd0af20129898acac3f769
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-03-30 15:44:04 +0900

    .

--- java01t/src/step18/Test2.sql
+++ java01t/src/step18/Test2.sql
@@ -34,7 +34,45 @@ CREATE TABLE test2 (
 INSERT INTO test2(tno, title, writer) VALUES(1, 'aaaa', 'hong');
 INSERT INTO test2(tno, title, writer) VALUES(2, 'bbbb', 'hong');
 INSERT INTO test2(tno, title, writer) VALUES(3, 'cccc', 'hong');
- 
+
+/* 3. 데이터 조회하기
+ * SELECT 컬럼명, 컬럼명, ... FROM 테이블명;
+ * 
+ * 예1)
+ */
+SELECT tno, title, writer FROM test2;
+
+
+/* 4. 데이터 변경하기
+ * UPDATE 테이블명 SET 컬럼명 = 변경할 값, 컬럼명 = 변경할 값, ... 
+ * WHERE 컬럼명 = 조건 
+ * 
+ * 예1)
+ */
+UPDATE test2 SET writer = 'bbb@test.com' WHERE tno = 1;
+UPDATE test2 SET writer = 'ccc@test.com' WHERE tno = 3;
+
+/* 5. 데이터 삭제하기
+ * DELETE FROM 테이블명 WHERE 컬럼명 = 조건;
+ * 
+ * 예1)
+ */
+DELETE FROM test2 WHERE tno = 2;
+
+/* 6. 테이블 삭제하기
+ * DROP TABLE 테이블명;
+ * 
+ * 예1)
+ */
+DROP TABLE test2;
+
+
+
+
+
+
+
+
 
 
 
