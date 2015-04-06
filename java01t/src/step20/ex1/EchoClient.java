a1daadc27896672b5edb1a28129c93e210d06aa1
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-04-06 14:49:35 +0900

    ...

--- java01t/src/step20/ex1/EchoClient.java
+++ java01t/src/step20/ex1/EchoClient.java
@@ -1,6 +1,8 @@
 package step20.ex1;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 // Client : 먼저 요청하는 쪽
 // Server : 요청 받는 쪽 
@@ -10,10 +12,25 @@ public class EchoClient {
   public static void main(String[] args) throws Exception {
     //1. 서버 소켓과 연결을 시도한다.
     //=> 서버 소켓과 연결되면 통신을 담당할 소켓 객체를 리턴한다.
-    Socket socket = new Socket("localhost", 8888);
+    Socket socket = new Socket("192.168.103.36", 8888);
     System.out.println("서버 소켓과 연결됨!");
     
-    // 데이터를 보내고 받기 
+    // 데이터를 보내고 받기
+    //2. 데이터를 편히 읽고 쓰기 위해 입출력 스트림 객체 준비
+    Scanner in = new Scanner(socket.getInputStream());
+    PrintStream out = new PrintStream(socket.getOutputStream());
+    
+    //3. 데이터 보내기 => 데이터를 모두 보낼 때까지 리턴하지 않는다.(blocking)
+    out.println("안녕하세요.^^");
+    
+    //4. 데이터 받기 => 줄 바꿈 데이터를 만날 때까지 리턴하지 않는다.(blocking)
+    String message = in.nextLine();
+    System.out.println("서버 => " + message);
+    
+    //5. 입출력이 끝났으면 닫아라!
+    in.close();
+    out.close();
+    
     
     //서버 소켓과 연결 끊기
     socket.close();
--- java01t/src/step20/ex1/EchoServer.java
+++ java01t/src/step20/ex1/EchoServer.java
@@ -1,7 +1,9 @@
 package step20.ex1;
 
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 // 서버 소켓: 요청을 받는 기능
 // 포트 번호: 데이터를 주고 받는 소켓을 구분하기 위해 붙이는 고유 번호
@@ -20,6 +22,7 @@ public class EchoServer {
   public static void main(String[] args) throws Exception {
     //1.서버 소켓 생성 
     ServerSocket ss = new ServerSocket(8888); //포트 번호 등록 
+    System.out.println("서버 소켓 준비 완료! 클라이언트 연결 기다림.");
     
     //2.클라이언트의 연결을 승인하라!
     //=> 클라이언트가 연결을 요청하면 대기열에 등록된다.
@@ -28,10 +31,22 @@ public class EchoServer {
     // 대기열에 클라이언트 정보가 들어 올 때까지 기다린다.(blocking)
     // 대기열에서 클라이언트 정보를 꺼내는 순간, 클라이언트와의 통신을 담당할 객체를 리턴한다.
     Socket socket = ss.accept(); 
-    
+    System.out.println("클라이언트 연결 승인!");
     
     // 데이터 읽고 보내기
+    //3. 데이터를 편히 읽고 쓰기 위해 입출력 스트림 객체 준비
+    Scanner in = new Scanner(socket.getInputStream());
+    PrintStream out = new PrintStream(socket.getOutputStream());
     
+    // 처음에 클라이언트에서 데이터를 보내기 때문에, 서버에서는 먼저 읽어야 한다.
+    String data = in.nextLine();
+    
+    // 클라이언트에게 데이터 보내기(응답한다).
+    out.println(data + ":엄진영");
+    
+    //4. 데이터 통신을 마친 다음에 자원을 해제한다.
+    in.close();
+    out.close();
     
     // 클라이언트 소켓과 연결 끊기
     socket.close();
