88118981d60f4451eacd7426b1f734ff0d86ebe5
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-04-07 11:59:04 +0900

    .

--- java01t/src/step20/ex5/CalcServer.java
+++ java01t/src/step20/ex5/CalcServer.java
@@ -3,7 +3,17 @@ package step20.ex5;
 public class CalcServer {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    /* 실행 순서
+     * 1. 클라이언트의 연결이 들어오면 승인한다.
+     * 2. 입출력 스트림을 준비한다.
+     * 3. 클라이언트가 보낸 계산 식을 읽는다.
+     * 4. 계산 식의 내용이 "quit"이라면, 
+     *    클라이언트에게 "goodbye"를 보낸다.
+     *    종료한다.
+     * 5. 공백을 기준으로 값을 분리한다.
+     * 6. 계산을 수행한 후 결과를 클라이언트로 보낸다.
+     * 7. 3번으로 간다. 
+     */
 
   }
 
