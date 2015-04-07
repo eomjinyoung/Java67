90782f462f12960e322f9262dcbbfc8c4eef61c6
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-04-07 11:47:22 +0900

    .

---
+++ java01t/src/step20/ex5/CalcClient.java
@@ -0,0 +1,21 @@
+package step20.ex5;
+
+public class CalcClient {
+
+  public static void main(String[] args) {
+    /* 실행 순서
+     * 0. 키보드 스캐너 준비
+     * 1. 서버에 연결
+     * 2. 입출력 스트림 준비 (Scanner, PrintStream)
+     * 3. 사용자로부터 계산 식을 입력 받는다.
+     * 4. 만약 입력 받은 값이 "quit"이라면 종료한다.
+     * 5. 서버에 전송한다.
+     * 6. 서버의 응답결과를 출력한다.
+     * 7. 3번으로 간다.
+     */
+    
+    
+
+  }
+
+}
---
+++ java01t/src/step20/ex5/CalcServer.java
@@ -0,0 +1,10 @@
+package step20.ex5;
+
+public class CalcServer {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
