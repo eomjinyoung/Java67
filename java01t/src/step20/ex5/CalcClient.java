bc6c9f05c710cd22c6f629ba32b28accd4c55791
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-04-07 14:35:06 +0900

    .

--- java01t/src/step20/ex5/CalcClient.java
+++ java01t/src/step20/ex5/CalcClient.java
@@ -1,8 +1,12 @@
 package step20.ex5;
 
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class CalcClient {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception{
     /* 실행 순서
      * 0. 키보드 스캐너 준비
      * 1. 서버에 연결
@@ -13,9 +17,41 @@ public class CalcClient {
      * 6. 만약 입력 받은 값이 "quit"이라면 종료한다.
      * 7. 3번으로 간다.
      */
-    
-    
-
+     Scanner scan = new Scanner(System.in);
+     Socket socket = null;
+     Scanner in = null;
+     PrintStream out = null;
+     
+     try {
+       socket = new Socket("localhost", 9999);
+       in = new Scanner(socket.getInputStream());
+       out = new PrintStream(socket.getOutputStream());
+       
+       String input;
+       String result;
+       
+       while (true) {
+         
+         System.out.print("계산식 입력 : ");
+         input = scan.nextLine();
+         
+         out.println(input);
+         result = in.nextLine();
+         
+         System.out.print("결과 : ");
+         System.out.println(result);
+         System.out.println();
+         
+         if(input.equals("quit")) 
+           break;
+       }
+       
+     } finally {
+       try {scan.close();} catch (Exception e) {}
+       try {in.close();} catch (Exception e) {}
+       try {out.close();} catch (Exception e) {}
+       try {socket.close();} catch (Exception e) {}
+     }
   }
 
 }
--- java01t/src/step20/ex5/CalcServer.java
+++ java01t/src/step20/ex5/CalcServer.java
@@ -1,8 +1,13 @@
 package step20.ex5;
 
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class CalcServer {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     /* 실행 순서
      * 1. 클라이언트의 연결이 들어오면 승인한다.
      * 2. 입출력 스트림을 준비한다.
@@ -14,6 +19,60 @@ public class CalcServer {
      * 6. 계산을 수행한 후 결과를 클라이언트로 보낸다.
      * 7. 3번으로 간다. 
      */
+    
+    ServerSocket ss = null;
+    Socket socket = null;
+    Scanner in = null;
+    PrintStream out = null;
+    
+    try {
+      ss = new ServerSocket(9999);
+      socket = ss.accept();
+      in = new Scanner(socket.getInputStream());
+      out = new PrintStream(socket.getOutputStream());
+      
+      String expr;
+      String[] exprItems;
+      
+      int v1 = 0, v2= 0, result = 0;
+      String oper;
+      
+      while (true) {
+        expr = in.nextLine();
+        if (expr.equals("quit")) {
+          out.print("goodbye");
+          break;
+        }
+        
+        exprItems = expr.split(" ");
+        
+        if (exprItems.length != 3) {
+          out.println("식이 올바르지 않습니다!");
+          continue;
+        }
+        
+        v1 = Integer.parseInt(exprItems[0]);
+        v2 = Integer.parseInt(exprItems[2]);
+        oper = exprItems[1];
+        
+        switch (oper) {
+        case "+": result = v1 + v2; break;
+        case "-": result = v1 - v2; break;
+        case "*": result = v1 * v2; break;
+        case "/": result = v1 / v2; break;
+        default:
+          out.println("지원하지 않는 연산자입니다.");
+          continue;
+        }
+        out.println(result);
+      }
+      
+    } finally {
+      try {in.close();} catch (Exception e) {}
+      try {out.close();} catch (Exception e) {}
+      try {socket.close();} catch (Exception e) {}
+      try {ss.close();} catch (Exception e) {}
+    }
 
   }
 
