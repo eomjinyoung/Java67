0eb1594393279ef31e3f46fcc8d605de6a78b3e0
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-04-24 12:26:27 +0900

    .

--- node1t/src/ex2/test01.js
+++ node1t/src/ex2/test01.js
@@ -1,3 +1,40 @@
 // 실습 목표
 console.log("실습목표", "mysql 서버에서 board2 select하기");
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host    :'localhost',
+    port : 3306,
+    user : 'java67',
+    password : 'java67',
+    database:'java67db'
+});
+
+connection.connect();
+
+connection.query(
+  "select bno, title, date_format(cre_date,'%Y-%m-%d') as cdate from board2", 
+  function(err,rows){
+	if (err){
+	  console.log(err);
+	  return;
+	} 
+	
+	for (var i in rows) {
+	  console.log(rows[i].bno, rows[i].title, rows[i].cdate);
+	}
+		
+	connection.end();
+});
+
+
+
+
+
+
+
+
+
+
+
