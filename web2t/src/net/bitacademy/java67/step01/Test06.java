5b9df29334c596ac088a0e2d93ec9063b040c49d
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-05-07 15:31:48 +0900

    .

--- web2t/src/net/bitacademy/java67/step01/Test06.java
+++ web2t/src/net/bitacademy/java67/step01/Test06.java
@@ -21,6 +21,13 @@ import javax.servlet.annotation.WebServlet;
   - 작업파일 
      /WebContent/step01/Test06.html
      /src/net/bitacademy/java67/step01/Test06.java
+  
+ * URL Encoding
+ * - 서버에서 HTTP 프로토콜을 분석하는데 혼란을 야기하는 문자는 특별한 형식으로 변환하여 보낸다.
+ * - 어떤 문자: &, =, ?, %, 공백, +, 다중 바이트 문자(한글,한자,일본글자, ...)
+ *   공백(+), =(%3D), ?(%3F), %(%25), &(%26), +(%2B), 가(%EA%B0%80), ...
+ * - 서버에서는 클라이언트가 인코딩한 데이터를 자동으로 디코딩한다.(개발자가 직접 할 필요가 없다)
+ * 
  */
 @WebServlet("/step01/Test06")
 public class Test06 extends GenericServlet {
@@ -31,11 +38,18 @@ public class Test06 extends GenericServlet {
       throws ServletException, IOException {
     request.setCharacterEncoding("UTF-8"); 
     
-    String name = request.getParameter("name");
-    int age = Integer.parseInt( request.getParameter("age") );
-
-    System.out.println(name);
-    System.out.println(age);
+    String op = request.getParameter("op"); // URL 인코딩된 데이터를 디코딩 하여 리턴한다. 
+    int a = Integer.parseInt( request.getParameter("a") );
+    int b = Integer.parseInt( request.getParameter("b") );
+    int result = 0;
+    
+    switch (op) { 
+    case "+" : result = a + b; break;
+    case "-" : result = a - b; break;
+    case "*" : result = a * b; break;
+    case "/" : result = a / b; break;
+    case "%" : result = a % b; break;
+    }
     
     response.setContentType("text/html;charset=UTF-8");
     
@@ -45,8 +59,7 @@ public class Test06 extends GenericServlet {
     out.println("    <title>step01/test04</title>");
     out.println("  </head>");
     out.println("  <body>");
-    out.println("  <p>이름: " + name + "</p>");
-    out.println("  <p>나이: " + age + "</p>");
+    out.println(a + " " + " " + op + " " + b + " = " + result);
     out.println("  </body>");
     out.println("</html>");
   }
