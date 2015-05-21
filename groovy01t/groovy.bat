345eae90defcf49ca903fd80483dd45e21d712e6
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-05-21 10:48:25 +0900

    .

--- groovy01t/groovy.bat
+++ groovy01t/groovy.bat
@@ -1,2 +1,2 @@
-SET GROOVY_HOME=/Users/eomjinyoung/bit/eclipse/plugins/org.codehaus.groovy_2.3.10.xx-201502281941-e44
+SET GROOVY_HOME=c:\bit\eclipse\plugins\org.codehaus.groovy_2.3.10.xx-201502281941-e44
 SET CLASSPATH=%GROOVY_HOME%\lib\groovy-all-2.3.10.jar;%CLASSPATH%
