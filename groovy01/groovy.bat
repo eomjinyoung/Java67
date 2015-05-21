e6f5b7cf7559396a7a00c6a2618b69d444bb85bd
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-05-21 10:44:48 +0900

    .

--- groovy01/groovy.bat
+++
@@ -1,2 +0,0 @@
-SET GROOVY_HOME=/Users/eomjinyoung/bit/eclipse/plugins/org.codehaus.groovy_2.3.10.xx-201502281941-e44
-SET CLASSPATH=%GROOVY_HOME%\lib\groovy-all-2.3.10.jar;%CLASSPATH%
--- groovy01/src/step01/Test01.groovy
+++
@@ -1,9 +0,0 @@
-package step01
-
-public class Test01 {
-
-  public static void main(String[] args) {
-    System.out.println("오호라...");
-  }
-
-}
