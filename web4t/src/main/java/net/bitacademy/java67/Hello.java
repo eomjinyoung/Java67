f56d58b10689e43d2f9d9b44b45c9c5edaa800ed
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-05-22 11:51:00 +0900

    .

---
+++ web4t/src/main/java/net/bitacademy/java67/Hello.java
@@ -0,0 +1,15 @@
+package net.bitacademy.java67;
+
+public class Hello {
+
+  public static void main(String[] args) {
+    // 한글 주석
+
+    switch ("aaa") {
+    case "aaa": System.out.println("오호라");break;
+    default:
+      System.out.println("okok");
+    }
+  }
+
+}
