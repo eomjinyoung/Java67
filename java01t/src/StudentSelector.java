3aa5f7326a3c527eb7b401320ce8da77b6560999
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-05-22 12:33:52 +0900

    .

--- java01t/src/StudentSelector.java
+++ java01t/src/StudentSelector.java
@@ -3,10 +3,8 @@ public class StudentSelector {
 
   public static void main(String[] args) throws Exception {
     String[] students = {
-        "김찬기",
-        "이혜인",
-        
-        "정병근","이서준"};
+        "정병근","이혜인","김희인"
+        };
     /*
     String[] students = {"배광한","심기백","고유식","안병래","황명헌",
         "심훈","남경훈","원석환","김찬기","박재민",
@@ -16,14 +14,14 @@ public class StudentSelector {
     */
     int no = 0; 
     
-    for (int i = 0; i < 10; i++) {
-      no = (int)(Math.random() * 4);
+    for (int i = 0; i < 20; i++) {
+      no = (int)(Math.random() * 3);
       //System.out.print(".");
       System.out.println(students[no]);
       Thread.sleep(200);
     }
-    Thread.sleep(5000);
-    System.out.println(students[no]);
+    //Thread.sleep(5000);
+    //System.out.println(students[no]);
 
   }
 
