088a6bd20187d10db1744610e75f46abdf1ccb34
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-05-26 14:59:37 +0900

    .

--- spring01t/src/main/java/step03/Test02.java
+++ spring01t/src/main/java/step03/Test02.java
@@ -3,22 +3,20 @@ package step03;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/* 실습 목표: 의존 객체 주입하기 */
+/* 실습 목표: 익명 의존 객체 주입하기 */
 
-public class Test01 {
+public class Test02 {
 
   public static void main(String[] args) {
     ApplicationContext context =
         new ClassPathXmlApplicationContext(
-            new String[] {"step03/application-context01.xml"});
+            new String[] {"step03/application-context02.xml"});
 
     Car car1 = (Car)context.getBean("car1");
     Car car2 = (Car)context.getBean("car2");
-    Car car3 = (Car)context.getBean("car3");
     
     System.out.printf("%s\n", car1);
     System.out.printf("%s\n", car2);
-    System.out.printf("%s\n", car3);
     
     System.out.printf("%b\n", car1.getEngine() == car2.getEngine());
   }
