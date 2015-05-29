fc5a26cb6aee06147b22443723fad1140bfa4011
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-05-29 15:24:03 +0900

    .

---
+++ web2t/src/net/bitacademy/java67/step17/CookieServlet.java
@@ -0,0 +1,74 @@
+package net.bitacademy.java67.step17;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/* 실습 목표: 클라이언트에 쿠키 보내기
+ * - 쿠키 값은 반드시 문자열이어야 한다.
+ * - HTTP 응답 프로토콜의 헤더 값으로 보내기 때문에 객체를 보낼 수 없다.
+ * 
+ */
+@WebServlet("/step17/cookie01")
+public class CookieServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  
+  @Override
+  protected void service(
+      HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    
+    //1) 쿠키 생성
+    Cookie cookie = new Cookie("name", "hongkildong");
+    Cookie cookie2 = new Cookie("age", "20");
+    
+    response.addCookie(cookie);
+    response.addCookie(cookie2);
+    
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("쿠키 보냄!!");
+    
+    /* HTTP 응답 프로토콜의 예
+HTTP/1.1 200 OK
+Server: Apache-Coyote/1.1
+Set-Cookie: name=hongkildong     <-------- 서버에서 클라이언트로 쿠키를 보내는 방법 
+Set-Cookie: age=20               <-------- 서버에서 클라이언트로 쿠키를 보내는 방법
+Content-Type: text/plain;charset=UTF-8
+Content-Length: 16
+Date: Fri, 29 May 2015 05:58:41 GMT 
+     */
+    
+    /* 클라이언트 측 행동 
+     * - 서버로부터 쿠키를 받으면, 타임아웃 시간이 설정되었는지 조사한다.
+     * - 만약 타임아웃 시간이 설정되지 않았다면,
+     *   => 메모리에 쿠키 값을 보관한다.
+     *   => 웹 브라우저가 종료되면 메모리에 보관된 쿠키 값도 삭제된다.
+     *   => 곧 웹 브라우저가 실행되는 동안만 쿠키 정보는 유지된다.
+     * - 만약 타임아웃 시간이 설정되었다면,
+     *   => 외부 저장장치(HDD)에 보관한다. 웹 브라우저마다 쿠키를 보관하는 폴더가 있다.
+     *   => 타임아웃이 끝나지 않았다면, 서버에 요청할 때마다 전달한다.
+     *   => 웹 브라우저를 종료했다가 다시 실행하더라도 쿠키 정보를 유지된다.
+     *   => 웹 브라우저는 서버에 요청할 때,
+     *      쿠키의 타임아웃을 조사하여 시간이 경과된 쿠키는 삭제한다.
+     */
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ web2t/src/net/bitacademy/java67/step17/OtherServlet01.java
@@ -0,0 +1,63 @@
+package net.bitacademy.java67.step17;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/* 실습 목표: 클라이언트가 보낸 쿠키 꺼내기
+ * 
+ */
+@WebServlet("/step17/other01")
+public class OtherServlet01 extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  
+  @Override
+  protected void service(
+      HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("/step17/other01");
+
+    //1) 쿠키 꺼내기
+    //- 요청 프로토콜에서 Cookie 헤더 값을 꺼낸다.
+    Cookie[] cookies = request.getCookies();
+
+    //2) 쿠키 값 출력하기
+    if (cookies != null) {
+      for (Cookie c : cookies) {
+        out.printf("%s=%s\n", c.getName(), c.getValue());
+      }
+    }
+    
+    /* HTTP 요청 프로토콜에 포함된 쿠키 정보 
+GET /web2t/step17/other01 HTTP/1.1
+Host: localhost:9999
+Connection: keep-alive
+Cache-Control: max-age=0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*|*;q=0.8
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36
+Accept-Encoding: gzip, deflate, sdch
+Accept-Language: ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4
+Cookie: name=hongkildong; age=20      <---- 요청할 때마다 서버로부터 받은 쿠키 정보를 보낸다.
+     */
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
