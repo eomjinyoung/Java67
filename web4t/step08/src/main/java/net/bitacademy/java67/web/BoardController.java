862fadc28c6351f19fff73a08356c587e44c650b
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-06-12 14:10:33 +0900

    .

--- web4t/step08/src/main/java/net/bitacademy/java67/web/BoardController.java
+++ web4t/step08/src/main/java/net/bitacademy/java67/web/BoardController.java
@@ -1,4 +1,4 @@
-package net.bitacademy.java67.web.ajax;
+package net.bitacademy.java67.web;
 
 import java.util.HashMap;
 
@@ -7,23 +7,27 @@ import net.bitacademy.java67.domain.BoardVo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/* 실습 목표: ContentNegotiatingViewResolver를 사용하기 */
+/* 실습 목표: CRUD 기능을 한 클래스에 넣기 */
 
-@Controller("/board/ajax")
-@RequestMapping("/board/ajax")
+@Controller
+@RequestMapping("/board")
 public class BoardController {
+  // 이제 BoardDao는 인터페이스이다.
+  // boardDao에 주입되는 것은 mybatis가 만든 BoardDao 구현체이다.
   @Autowired
   BoardDao boardDao;
   
   @RequestMapping("/list")
-  public Object list(
+  public String list(
       @RequestParam(required=false,defaultValue="1") int pageNo, 
       @RequestParam(required=false,defaultValue="3") int pageSize, 
       @RequestParam(required=false) String word, 
-      @RequestParam(required=false) String order) throws Exception {
+      @RequestParam(required=false) String order, 
+      Model model) throws Exception {
 
     // BoardDao 인터페이스의 selectList()는 한 개의 파라미터를 요구한다.
     // 따라서 SQL 파라미터 값을 맵 객체에 담아서 넘겨야 한다.
@@ -33,17 +37,13 @@ public class BoardController {
     sqlParams.put("word", word);
     sqlParams.put("order", order);
     
+    model.addAttribute("list", boardDao.selectList(sqlParams));
+    model.addAttribute("pageNo", pageNo);
+    model.addAttribute("pageSize", pageSize);
+    model.addAttribute("maxPage", countTotalPage(pageSize, 
+                                     boardDao.countAll(word)));
     
-    // 클라이언트에 보낼 데이터를 담을 맵 객체 준비
-    HashMap<String,Object> responseData = new HashMap<String,Object>();
-    responseData.put("status", "success");
-    responseData.put("data", boardDao.selectList(sqlParams));
-    responseData.put("pageNo", pageNo);
-    responseData.put("pageSize", pageSize);
-    responseData.put("maxPage", countTotalPage(pageSize, 
-                                    boardDao.countAll(word)));
-    
-    return responseData;
+    return "board/BoardList";
   }
 
   private int getStartIndexOfPage(int pageNo, int pageSize) {
@@ -59,42 +59,31 @@ public class BoardController {
   }
   
   @RequestMapping("/add")
-  public Object add(BoardVo board) throws Exception {
+  public String add(BoardVo board) throws Exception {
     boardDao.insert(board);
     
-    HashMap<String,Object> responseData = new HashMap<String,Object>();
-    responseData.put("status", "success");
-    
-    return responseData;
+    return "redirect:list.do";
   }
   
   @RequestMapping("/change")
-  public Object change(BoardVo board) throws Exception {
+  public String change(BoardVo board) throws Exception {
     boardDao.update(board);
     
-    HashMap<String,Object> responseData = new HashMap<String,Object>();
-    responseData.put("status", "success");
-    
-    return responseData;
+    return "redirect:list.do";
   }
   
   @RequestMapping("/delete")
-  public Object delete(int no) throws Exception {
+  public String delete(int no) throws Exception {
     boardDao.delete(no);
     
-    HashMap<String,Object> responseData = new HashMap<String,Object>();
-    responseData.put("status", "success");
-    
-    return responseData;
+    return "redirect:list.do";
   }
   
   @RequestMapping("/detail")
-  public Object detail(int no) throws Exception {
-    HashMap<String,Object> responseData = new HashMap<String,Object>();
-    responseData.put("status", "success");
-    responseData.put("data", boardDao.selectOne(no));
+  public String detail(int no, Model model) throws Exception {
+    model.addAttribute("board", boardDao.selectOne(no));
     
-    return responseData;
+    return "board/BoardDetail";
   }
 
 }
---
+++ web4t/step08/src/main/java/net/bitacademy/java67/web/filter/JsonFilter.java
@@ -0,0 +1,45 @@
+package net.bitacademy.java67.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("*.do")
+public class JsonFilter implements Filter {
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {}
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+      FilterChain chain) throws IOException, ServletException {
+    
+    HttpServletRequest httpRequest = (HttpServletRequest)request;
+    
+    if (httpRequest.getServletPath().contains("/ajax")) {
+      HttpServletResponse httpResponse = (HttpServletResponse)response;
+      httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+    }
+    
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {}
+
+}
+
+
+
+
+
+
+
